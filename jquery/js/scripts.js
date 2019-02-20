dfa6916dbd0bcd0424b20fd90dd15ac81a0512d5
IanSani <iansani259@gmail.com>
2019-02-20 10:42:24 +0300

    Add function in scripts.js to dispal image

---
+++ jquery/js/scripts.js
@@ -0,0 +1,11 @@
+jquery(h1).click(function(){
+  alert("This is a heading tag ")
+});
+
+jquery("p").click(function(){
+  alert("This is a paragraph")
+});
+
+jquery("img").click(function(){
+  alert("This is an image")
+});
